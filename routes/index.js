4cfcb777348fb4b5765f95601ca4d6a0c70b54cb
Brittany Fisher <bribreana7@gmail.com>
2021-07-27 10:39:55 -0400

    removed all renders and replaced wit res json, converted to api, deleted template

--- routes/index.js
+++ routes/index.js
@@ -1,74 +1,50 @@
-'use strict';
+"use strict";
 
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const slugify = require('slugify');
-const {ceos} = require('../models');
+const slugify = require("slugify");
+const { ceos } = require("../models");
 
-router.get('/:slug?', async (req, res) => {
+router.get("/:slug?", async (req, res) => {
+  if (!!req.params.slug) {
+    const { slug } = req.params;
+    const theCEO = await ceos.findOne({ where: { slug } });
+    console.log("THE CEO IS:", theCEO);
 
-    if (!!req.params.slug) {
-        const { slug } = req.params;
-        const theCEO = await ceos.findOne({where: {slug}});
-        console.log('THE CEO IS:', theCEO);
-
-
-        res.render('template', {
-            locals: {
-                title: 'CEO DETAILS',
-                ceo: theCEO
-            },
-            partials: {
-                body: 'partials/ceo-details',
-            },
-        });
-    } else {
-        const ExecutiveData = await ceos.findAll();
-        console.log(ExecutiveData);
-        res.render('template', {
-            locals: {
-                title: 'Home Page',
-                data: ExecutiveData
-            },
-            partials: {
-                body: 'partials/home',
-            },
-            
-            });
-    
-}
-    
+    res.json(theCEO).status(200);
+  } else {
+    const ExecutiveData = await ceos.findAll();
+    console.log(ExecutiveData);
+    res.json(ExecutiveData).status(200);
+  }
 });
 
-router.post('/', async (req, res) => {
-   
-    const { ceo_name, ceo_year } = req.body;
+router.post("/", async (req, res) => {
+  const { ceo_name, ceo_year } = req.body;
 
-    const slug = slugify(ceo_name, {
-        replacement: '_',
-        lower: true,
-        strict: true
-    });
+  const slug = slugify(ceo_name, {
+    replacement: "_",
+    lower: true,
+    strict: true,
+  });
 
-    const response = await ceos.create({
-        name:ceo_name,
-        slug: slug,
-        first_year_active:ceo_year
-    });
+  const response = await ceos.create({
+    name: ceo_name,
+    slug: slug,
+    first_year_active: ceo_year,
+  });
 
-    console.log('RESPONSE FROM CEO CREATE:', response);
-    res.redirect('/');
-
-    
+  console.log("RESPONSE FROM CEO CREATE:", response);
+  res.redirect("/");
 });
 
-router.post('/delete', async (req,res) => {
-    const{id, ceo_name, slug, ceo_year} = req.body;
+router.post("/delete", async (req, res) => {
+  const { id, ceo_name, slug, ceo_year } = req.body;
 
-    const response = await ceos.destroy({
-        where:{id:id},
-    });
-    console.log('DELETE ROUTE RESPONSE IS:', response);
-    res.redirect('/');
+  const response = await ceos.destroy({
+    where: { id: id },
+  });
+  console.log("DELETE ROUTE RESPONSE IS:", response);
+  res.redirect("/");
 });
 module.exports = router;
